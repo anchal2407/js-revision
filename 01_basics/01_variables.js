7614a6dd858c54fc2793c40e04f1145242517395
Anchal Singh <123294998+anchal2407@users.noreply.github.com>
2026-03-18 16:08:54 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 240702
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1312434"
+accountCity = "Ahmedabad"
+let accountName;
+accountEmail = "changed@gmail.com"
+accountPassword = "00000"
+accountCity = "Mumbai"
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountName])
